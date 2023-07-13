6a88dc41195703c4cd44f3d95f04023c58773959
josh <joshmarcus85@gmail.com>
2023-07-13 10:16:46 -0600

    Added workflow deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ export default defineConfig({
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
-      ? 'https://brutal.elian.codes/'
+      ? 'https://jmarquee85.github.io'
       : process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3001/',
+  base: 
+    '/mgh_brutal'
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
