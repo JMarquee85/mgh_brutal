bbe658a53c25e85f6c67964b800ed8abc907fd6d
josh <joshmarcus85@gmail.com>
2023-07-15 17:41:49 -0600

    Change to config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site:
-    process.env.VERCEL_ENV === 'production'
-      ? 'https://jmarquee85.github.io'
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/`
-      : 'https://localhost:3001/',
-  base: 
-    '/mgh_brutal'
+  site: 'https://jmarquee85.github.io'
+  base: '/mgh_brutal'
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
 });
